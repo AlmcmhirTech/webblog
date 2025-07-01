67150a1ad61f73ad81b469eb9d7af12b8aabf3a1
AlmcmhirTech <almcmhirservices@gmail.com>
2025-07-01 09:42:11 +0800

    yse

---
+++ webblog/src/blog/sections.tsx
@@ -0,0 +1,10 @@
+export function Sections(){
+    return(
+        <div className="w-screen h-screen">
+            <div className="flex w-full h-full max-h-[400px] bg-[url('/src/assets/manilabackground.jpg')] bg-no-repeat bg-cover">
+                <h1 className="m-auto text-[#FF6B6B]">My Summer Educational Tour</h1>
+            </div>
+        </div>
+    )
+}
+
