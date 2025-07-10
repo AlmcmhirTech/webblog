8736c00083a2a91e2b15b842c7539061e873debd
AlmcmhirTech <almcmhirservices@gmail.com>
2025-07-10 19:05:13 +0800

    das

--- src/blog/content/home.tsx
+++ src/blog/content/home.tsx
@@ -9,7 +9,7 @@ export function Homepage(){
                 <div className="flex flex-col justify-center absolute top-60 left-1/2 -translate-x-1/2">
                     <h1 className="font-['Roboto'] font-bold text-[#E5D2B8]">Off Ice Summers are the best!</h1>
                     
-                    <button className="m-auto mt-8 bg-[#373737]"><a className="font-['Roboto'] font-bold text-white text-lg" href="/webblog/travels">Figure out why...</a></button>
+                    <button className="m-auto mt-8 bg-[#373737]"><a className="font-['Roboto'] font-bold text-white text-lg" href="/webblog/#/travels">Figure out why...</a></button>
                 </div>
             </div>
         </div>
--- src/blog/content/travels.tsx
+++ src/blog/content/travels.tsx
@@ -1,3 +1,5 @@
+import industryimmersion from '../../assets/industryimmersion.jpg'
+
 export function TravelsList(){
     return(
         <div className="w-full">
@@ -8,8 +10,7 @@ export function TravelsList(){
                     <div className="">
                         <div className="hover:bg-[#c5b6a1] p-4">
                             <a href="/webblog/tour">
-                                <img className='shadow-md shadow-[#373737] opacity-90 brightness-80 mb-2' src="/webblog/src/assets/industryimmersion.jpg" alt="" />
-
+                                <img className='shadow-md shadow-[#373737] opacity-90 brightness-80 mb-2' src={industryimmersion} alt="" />
                                 <div>
                                     <span className="font-['Roboto'] font-bold text-[#373737] text-lg">BSCS Batch 2026 Industry Immersion</span><br />
                                     <span className="font-['Roboto'] text-[#373737]">June 08, 2025</span>
@@ -20,7 +21,7 @@ export function TravelsList(){
                      <div className="">
                         <div className="hover:bg-[#c5b6a1] p-4">
                             <a className='' href="">
-                                <img className='shadow-md shadow-[#373737] opacity-100 brightness-0 mb-2' src="/webblog/src/assets/industryimmersion.jpg" alt="" />
+                                <img className='shadow-md shadow-[#373737] opacity-100 brightness-0 mb-2' src={industryimmersion} alt="" />
 
                                 <div>
                                     <span className="font-['Roboto'] font-bold text-[#373737] text-lg">More Coming Soon!</span><br />
