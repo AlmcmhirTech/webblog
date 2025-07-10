a29cfb8a11ebe850e16237b24afa0fe09955bd60
AlmcmhirTech <almcmhirservices@gmail.com>
2025-07-10 18:06:28 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')!).render(
         <Header />
         <HashRouter>
           <Routes>
-            <Route path='/webblog'>
+            <Route path='/'>
               <Route index element={<Homepage />}/>
               <Route path="travels" element={<TravelsList />}/>
               <Route path="tour" element={<RenderIndustryImmersion />}/>
